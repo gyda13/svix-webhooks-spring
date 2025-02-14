696db7a3b4e095a3fd1c3891daa86e22bb151d22
gyda <galmohaimeed@rematadvanced.sa>
2025-02-14 21:38:41 +0300

    Initial commit

---
+++ src/test/java/com/example/svixwebhooksspring/SvixWebhooksSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.svixwebhooksspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvixWebhooksSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
