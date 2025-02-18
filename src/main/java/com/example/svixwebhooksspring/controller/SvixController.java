26f8c7e4f282df66b12a85353f0db5f04faac35f
gyda13 <gyda2100@gmail.com>
2025-02-18 20:01:36 +0300

    add Svix component for webhook integration

---
+++ src/main/java/com/example/svixwebhooksspring/controller/SvixController.java
@@ -0,0 +1,107 @@
+package com.example.svixwebhooksspring.controller;
+
+import com.example.svixwebhooksspring.service.SvixService;
+import com.svix.models.*;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller("/svix")
+@AllArgsConstructor
+public class SvixController {
+
+    private final SvixService svixService;
+
+    @PostMapping("/apps")
+    public ResponseEntity<ApplicationOut> createApp(
+            @RequestParam String appId,
+            @RequestParam String appName
+    ) {
+        return ResponseEntity.ok(svixService.createUserApp(appName, appId));
+    }
+
+    @GetMapping("/apps")
+    public ResponseEntity<List<ApplicationOut>> getApps() {
+        return ResponseEntity.ok(svixService.getAllUsersApp());
+    }
+
+    @GetMapping("/apps/{appId}")
+    public ResponseEntity<ApplicationOut> getApp(@PathVariable String appId) {
+        return ResponseEntity.ok(svixService.getUserApp(appId));
+    }
+
+    @GetMapping("/apps/{appId}/endpoints/{endpointId}/secret")
+    public ResponseEntity<EndpointSecretOut> getEndpointSecret(@PathVariable String appId, @PathVariable String endpointId) {
+        return ResponseEntity.ok(svixService.getEndpointSecret(appId, endpointId));
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<EventTypeOut> createEvent(
+            @RequestParam String event,
+            @RequestParam String description
+    ) {
+        return ResponseEntity.ok(svixService.createEvent(event, description));
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<List<EventTypeOut>> getEvents() {
+        return ResponseEntity.ok(svixService.getEvents());
+    }
+
+    @GetMapping("/apps/{appId}/endpoints")
+    public ResponseEntity<ListResponseEndpointOut> getEndpoints(@PathVariable String appId) {
+        return ResponseEntity.ok(svixService.getEndpoints(appId));
+    }
+
+    @GetMapping("/apps/{appId}/endpoints/{endpointId}")
+    public ResponseEntity<EndpointOut> getEndpoint(
+            @PathVariable String appId,
+            @PathVariable String endpointId
+    ) {
+        return ResponseEntity.ok(svixService.getEndpoint(appId, endpointId));
+    }
+
+    @PostMapping("/apps/{appId}/endpoints")
+    public ResponseEntity<EndpointOut> createEndpoint(
+            @PathVariable String appId,
+            @RequestParam String event
+            ) {
+        return ResponseEntity.ok(svixService.createEndpoint(appId, event));
+    }
+
+    @DeleteMapping("/apps/{appId}/endpoints/{endpointId}")
+    public ResponseEntity<Void> deleteEndpoint(
+            @PathVariable String appId,
+            @PathVariable String endpointId
+    ) {
+        svixService.deleteEndpoint(appId, endpointId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/send-webhooks/{appId}")
+    public ResponseEntity<MessageOut> sendWebhookMessage(
+            @PathVariable String appId,
+            @RequestParam String event,
+            @RequestBody Map<String, Object> payload
+    ) {
+        return ResponseEntity.ok(svixService.sendWebhookMessage(appId, event, payload));
+    }
+
+    @PostMapping("/send-webhooks")
+    public ResponseEntity sendWebhooksMessages(
+            @RequestParam String event,
+            @RequestBody Map<String, Object> payload
+    ) {
+        svixService.sendWebhooksMessages(event, payload);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/apps/{appId}/messages")
+    public ResponseEntity<List<MessageOut>> getAppMessages(@PathVariable String appId) {
+        return ResponseEntity.ok(svixService.getAppMessages(appId));
+    }
+}
---
+++ src/main/java/com/example/svixwebhooksspring/service/SvixService.java
@@ -0,0 +1,176 @@
+package com.example.svixwebhooksspring.service;
+
+import com.svix.*;
+import com.svix.exceptions.ApiException;
+import com.svix.models.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.*;
+
+@Service
+@Slf4j
+public class SvixService {
+
+    private final Svix svix;
+
+    public SvixService(@Value("${svix.token}") String svixToken, @Value("${svix.url}") String svixUrl) {
+        SvixOptions options = new SvixOptions();
+        options.setDebugUrl(svixUrl);
+        this.svix = new Svix(svixToken, options);
+    }
+
+    public ApplicationOut createUserApp(String appName, String appId) {
+        ApplicationOut appOut;
+        try {
+            appOut = svix.getApplication().create(
+                    new ApplicationIn()
+                            .name(appName)
+                            .uid(appId)
+            );
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+
+        return appOut;
+    }
+
+    public ApplicationOut getUserApp(String appId) {
+        ApplicationOut appOut;
+        try {
+            appOut = svix.getApplication().get(appId);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return appOut;
+    }
+
+    public List<ApplicationOut> getAllUsersApp() {
+        List<ApplicationOut> applicationOutList;
+        try {
+            applicationOutList = svix.getApplication().list(new ApplicationListOptions()).getData();
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return applicationOutList;
+    }
+
+    public EventTypeOut createEvent(String event, String description) {
+        EventTypeOut eventTypeOut;
+        try {
+            eventTypeOut = svix.getEventType().create(new EventTypeIn().name(event).description(description));
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return eventTypeOut;
+    }
+
+    public List<EventTypeOut> getEvents() {
+        List<EventTypeOut> eventTypeOutList;
+        try {
+            eventTypeOutList = svix.getEventType().list(new EventTypeListOptions()).getData();
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return eventTypeOutList;
+    }
+
+    public EndpointOut createEndpoint(String appId, String event) {
+        EndpointOut endpointOut;
+        Set<String> filterTypes = new HashSet<>();
+        filterTypes.add(event);
+        try {
+            endpointOut = svix.getEndpoint().create(appId,
+                    new EndpointIn()
+                            .uid(appId)
+                            .url(URI.create("https://play.svix.com/in/e_A83zvHoXsf6YRZiejkFlj2BjpWR/"))
+                            .disabled(false)
+                            .filterTypes(filterTypes)
+            );
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+
+        return endpointOut;
+    }
+
+    public ListResponseEndpointOut getEndpoints(String appId) {
+        ListResponseEndpointOut endpointOutList;
+        try {
+            endpointOutList = svix.getEndpoint().list(appId, new EndpointListOptions());
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return endpointOutList;
+    }
+
+    public EndpointOut getEndpoint(String appId, String endpointId) {
+        EndpointOut endpointOut;
+        try {
+            endpointOut = svix.getEndpoint().get(appId, endpointId);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return endpointOut;
+    }
+
+    public EndpointSecretOut getEndpointSecret(String appId, String endpointId) {
+        EndpointSecretOut endpointSecretOut;
+        try {
+            endpointSecretOut = svix.getEndpoint().getSecret(appId, endpointId);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        return endpointSecretOut;
+    }
+
+    public void deleteEndpoint(String appId, String endpointId) {
+        try {
+            svix.getEndpoint().delete(appId, endpointId);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public MessageOut sendWebhookMessage(String appId, String event, Map<String, Object> payload) {
+        MessageOut messageOut;
+        try {
+            messageOut = svix.getMessage().create(appId,
+                    new MessageIn()
+                            .eventType(event)
+                            .payload(payload));
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+
+        return messageOut;
+    }
+
+    public void sendWebhooksMessages(String event, Map<String, Object> payload) {
+        List<ApplicationOut> applications = getAllUsersApp();
+        for (ApplicationOut app: applications) {
+            try {
+                payload.put("appId", app.getUid());
+                svix.getMessage().create(app.getUid(),
+                        new MessageIn()
+                                .eventType(event)
+                                .payload(payload));
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public List<MessageOut> getAppMessages(String appId) {
+        List<MessageOut> messageOutList;
+        try {
+            messageOutList = svix.getMessage().list(appId, new MessageListOptions()).getData();
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+
+        return messageOutList;
+    }
+}
